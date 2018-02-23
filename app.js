86c94ed5c4c5503b4b54a9524076eefa766bf761
baehll <h@a.com>
2018-02-23 12:54:09 +0100

    Aufruf von Pythonskripte aus node (durch URI)

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var n2p     = require("./cm_modules/node2py");
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
+});
+
+app.get("/python", function(reg, res){
+  n2p.setPyExec("test.py");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(3000);
